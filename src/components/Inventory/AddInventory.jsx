3f7832bbf061d6b23f7f60c44739626f1c011e03
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-08-30 16:22:59 -0700

    added CSS for AddOrder.jsx

--- src/components/Inventory/AddInventory.jsx
+++ src/components/Inventory/AddInventory.jsx
@@ -37,7 +37,6 @@ export default function AddInventory() {
         onChange={handleQuantity}
       />
       </div>
-     
       <div>
       <SelectControl className="compstyle2"
             type={'string'}
