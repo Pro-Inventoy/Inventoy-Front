04f3721c3dc19a9cc97d0c8d2fe04d4c12e431f7
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-09-01 11:19:24 -0700

    new hook, got piechart on homepage rendering orders, styled navigation bar/app name

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -6,6 +6,9 @@ import client from '../../state/services/client';
 import { getNameOfUser, getUser } from '../../state/services/user-service';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { useOrderData } from "../../state/hooks/order.js";
+
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -37,7 +40,47 @@ export const data = {
 };
 
 export default function Homepage() {
+  const { orders, compOrders } = useOrderData();
   const { transactions } = useTransactions();
+
+  let currentData = [0,0]
+  if(orders){
+    for(let i = 0; i < orders.length; i++){
+      currentData[0] = currentData[0] + orders[i];
+    }
+    for(let i = 0; i < compOrders.length; i++){
+      currentData[1] = currentData[1] + compOrders[i];
+    }
+    
+
+  }
+  const newData = {
+    labels: ['Order', 'Order Filled'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: currentData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  }; 
+  
   // transactions is the 10 most recent transactions, most recent at [0]
   const ref = useRef({
     inv_subscription: null
@@ -82,7 +125,7 @@ useEffect(() => {
         </ul>
       </div>
       <div className="pie-size">
-      <Pie data={data} />
+      <Pie data={newData} />
       </div>
       
       
--- src/state/hooks/order.js
+++ src/state/hooks/order.js
@@ -73,3 +73,35 @@ export function useOrderActions() {
   });
   return { add, update, remove };
 }
+
+export function useOrderData() {
+  const [error, setError] = useState(null);
+  const [orders, setOrders] = useState(null);
+  const [compOrders, setCompOrders] = useState(null);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetch = async () => {
+      const { data, error } = await getOrders();
+      console.log(data);
+      const newData = data.map(order => order.orderquantity - order.completed);
+      const newData2 = data.map(order => order.completed);
+      if (ignore) return;
+
+      if (error) {
+        setError(error);
+      }
+      if (data) {
+        setOrders(newData);
+        setCompOrders(newData2);
+
+      }
+    };
+
+    fetch();
+    return () => (ignore = true);
+  }, []);
+
+  return { orders, compOrders, error };
+}
