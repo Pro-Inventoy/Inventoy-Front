4f1313f1503d9b449953a25f95e05376ab7f1602
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-09-02 10:04:37 -0700

    stylin on aboutus page

--- src/components/About us/Aboutus.jsx
+++ src/components/About us/Aboutus.jsx
@@ -4,7 +4,7 @@ export default function AboutUs() {
     return (
         <div className='Aboutus'>
         <div className='us'>
-            <img class='abtPic'alt=' Elliott Cheifetz' src='https://media-exp1.licdn.com/dms/image/C4E03AQHgBX_O214LTA/profile-displayphoto-shrink_800_800/0/1655133476394?e=1667433600&v=beta&t=ImC3xePD1tj5HOnbCg76CXqvFioMi16XFqcU8LEQ90s'></img>
+            <div class='abtPic1'alt=' Elliott Cheifetz'></div>
             <h1>Elliott Cheifetz</h1>
             <p> Graphic design and coding enjoyer who likes reading about history/politics and feeding the squirrels.</p>
             <br/>
@@ -13,7 +13,7 @@ export default function AboutUs() {
             <a href='https://www.linkedin.com/in/elliott-cheifetz/'>LinkedIn</a>
         </div>
         <div className='us'>    
-            <img class='abtPic' alt='Ian Cheifetz'src='https://media-exp1.licdn.com/dms/image/C4D03AQGH6s1OUHfTWg/profile-displayphoto-shrink_800_800/0/1651621609859?e=1667433600&v=beta&t=7nFZM9jsx6fSJ9-9Szd49FQ9zL9fi9JwTN5vuSDN3wQ'></img>
+            <div class='abtPic2' alt='Ian Cheifetz'></div>
             <h1>Ian Cheifetz</h1>
             <p> Backend enthusiast and blacksmithe and somebody enjoys being inna woods or producing digital art/3d printing. </p>
             <a href='https://github.com/IACheifetz'>Github</a>
@@ -21,7 +21,7 @@ export default function AboutUs() {
             <a href='https://www.linkedin.com/in/iancheifetz/'>LinkedIn</a>
         </div>
         <div className='us'>
-            <img class='abtPic' alt='Khayman King' src='https://media-exp1.licdn.com/dms/image/C5603AQFqpZNzpd8g6w/profile-displayphoto-shrink_800_800/0/1652400698520?e=1667433600&v=beta&t=-mIEBqDWWYpH52e-yIvyXS8Xw7GKkNwuF_ZbvrNwKO0'></img>
+            <div class='abtPic3' alt='Khayman King'></div>
             <h1>Khayman King</h1>
             <p> Just a goofy dude located in the Washington, who enjoys coding, cooking, and spending time outside.</p>
             <br/>
@@ -30,7 +30,7 @@ export default function AboutUs() {
             <a href='https://www.linkedin.com/in/khaymanaking/'>LinkedIn</a>
         </div>
         <div className='us'>
-            <img class='abtPic' alt='Thomas Harding' src='https://media-exp1.licdn.com/dms/image/C4E03AQFmJoeJW_OHnA/profile-displayphoto-shrink_800_800/0/1608411761363?e=1667433600&v=beta&t=5d2TzL195dSm-ki-xyXzygOlHrKdHaRvYNyDFXG5z9c'></img>
+            <div class='abtPic4' alt='Thomas Harding'></div>
             <h1>Thomas Harding</h1>
             <p> Backend dev and blue medalled wrangler of errant databases. Avid presser of Odyn's Fury.
 
